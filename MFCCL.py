78fa5751ddf8fd23133262385fab51cd6908266f
ChillierArk <100814626+ChillierArk@users.noreply.github.com>
2022-05-30 13:07:31 -0400

    Alpha-1.3
    + New Commnands

--- MFCCL.py
+++ MFCCL.py
@@ -4,6 +4,8 @@ import cryptography
 from cryptography.fernet import Fernet
 import string
 import webbrowser
+import math
+import statistics
 
 running = True
 root = False
@@ -26,8 +28,6 @@ def reset_maindata():
     with open('MainData.txt', 'w') as f:
         f.write('')
     print('MainData.txt wiped.')
-
-
 
 
 try:
@@ -117,6 +117,8 @@ root > Allows the user to retry root login.
 webbrowser > Opens the default web browser to google.
 *resetmaindata > WILL RESET ALL DATA STORED IN THIS FILE INCLUDING ROOT PASS AND OTHER SETTINGS. 
 *resetsavedpasswords > WILL RESET ALL DATA STORED IN THIS FILE.
+dataset > Calculates useful information about a data set.
+*createfile > Creates a file at a specified path with a specified name and extension.
         ''')
     elif command == 'q' or command == 'quit':
         print('Goodbye!')
@@ -269,12 +271,67 @@ webbrowser > Opens the default web browser to google.
             are_you_sure = input('Retype the command with proper capitilization.(ResetMainData) >>> ')
             if are_you_sure == 'ResetMainData':
                 reset_maindata()
+        else:
+            print('No Root Access')
 
     elif command == 'resetsavedpasswords':
         if root:
             are_you_sure = input('Retype the command with proper capitilization.(ResetSavedPasswords) >>> ')
             if are_you_sure == 'ResetSavedPasswords':
                 reset_passwords()
+        else:
+            print('No Root Access')
+
+    elif command == 'dataset':
+        file = input('Do you have a data file with all of the numbers on separate lines? (y/n) >>> ').lower()
+        if file == 'y' or file == 'yes':
+            file_name = input('What is the name of the file? >>> ')
+            file_extension = input('What is the file extension? (Should be txt) >>> ')
+            file_directory = input('Where is the file located? (All Slashes Need to Be included)(Optional) >>> ')
+            with open(f'{file_directory}{file_name}.{file_extension}', 'r') as f:
+                dataset = f.readlines()
+            dataset_temp = []
+            for num in dataset:
+                dataset_temp.append(int(num.strip()))
+            dataset = dataset_temp
+            dataset.sort()
+            print(f'The sum is {sum(dataset)}')
+            print(f'The mean is {statistics.mean(dataset)}')
+            print(f'The median is {statistics.median(dataset)}')
+            print(f'The mode is {statistics.mode(dataset)}')
+            print(f'The Standard Deviation is {statistics.stdev(dataset)}')
+            print(f'Your sorted list is {dataset}')
+        else:
+            data = []
+            amount = int(input('How many numbers do you have? >>> '))
+            for i in range(0, amount):
+                number_to_add = input(f'Number #{i + 1} >>> ')
+                if number_to_add.lower() == 'q' or number_to_add.lower() == 'quit':
+                    print('Stopped')
+                    break
+                else:
+                    data.append(float(number_to_add))
+            if number_to_add.lower() == 'q' or number_to_add.lower() == 'quit':
+                pass
+            else:
+                data.sort()
+                print(f'The sum is {sum(data)}')
+                print(f'The mean is {statistics.mean(data)}')
+                print(f'The median is {statistics.median(data)}')
+                print(f'The mode is {statistics.mode(data)}')
+                print(f'The Standard Deviation is {statistics.stdev(data)}')
+                print(f'Your sorted list is {data}')
+
+    elif command == 'createfile':
+        if root:
+            file_name = input('What do you want to name the file? >>> ')
+            file_extension = input('What do you want the file extension to be? >>> ')
+            file_directory = input('Where do you want the file to be? (All Slashes Need to Be included)(Optional) >>> ')
+            open(f'{file_directory}{file_name}.{file_extension}', 'w')
+            print(f'File {file_name}.{file_extension} created.')
+        else:
+            print('No Root Access')
+
 
     else:
         print('Not Understood, Please Try Again.')
